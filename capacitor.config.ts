9ac6c761004e3225845841e64b4a1717bddd0701
Robert Mengual <robert.mengual@outlook.com>
2021-11-22 18:04:37 +0100

    rename app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'open-valencia',
+  appName: 'open-pros-cons',
   webDir: 'build',
   bundledWebRuntime: false
 };
