2a015b9862283e76996aaaa7bc06b1d7b371152f
Robert Mengual <robert.mengual@outlook.com>
2021-12-09 20:16:28 +0100

    add silly white space

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
         <IonRouterOutlet>
           <Route path="/" exact={false}> */}
       <ProsAndConsPage />
-      {/* </Route>
+      {/*  </Route>
         </IonRouterOutlet>
       </IonReactRouter> */}
     </IonApp>
